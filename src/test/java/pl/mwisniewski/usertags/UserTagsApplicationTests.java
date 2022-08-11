f3d4506b14f7b93f3e81c75e95a06961921cf34e
Michal Wisniewski <michu123qwe@gmail.com>
2022-08-11 15:46:31 +0200

    Project Setup.

---
+++ src/test/java/pl/mwisniewski/usertags/UserTagsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mwisniewski.usertags;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTagsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
