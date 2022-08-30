23557ec65f4794039cbf015c891f947de227a194
questras <michal.wisniewski422@gmail.com>
2022-08-30 09:12:58 +0200

    Change topic retention.

--- src/main/java/pl/mwisniewski/usertags/adapters/kafka/KafkaTopicConfiguration.java
+++ src/main/java/pl/mwisniewski/usertags/adapters/kafka/KafkaTopicConfiguration.java
@@ -1,6 +1,7 @@
 package pl.mwisniewski.usertags.adapters.kafka;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,9 @@ public class KafkaTopicConfiguration {
     public NewTopic userTagTopic() {
         return TopicBuilder
                 .name(topicName)
+                .config(TopicConfig.RETENTION_MS_CONFIG, HOUR_MS.toString())
                 .build();
     }
+
+    private static final Integer HOUR_MS = 1 * 60 * 60 * 1000;
 }
